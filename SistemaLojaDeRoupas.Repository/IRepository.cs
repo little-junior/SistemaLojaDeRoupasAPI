539dcf896555e5ff0557f02b720b46f9d638b7e5
little-junior <rampagedolol@outlook.com>
2024-02-18 16:48:09 -0300

    feat: add repository interface

---
+++ SistemaLojaDeRoupas.Repository/IRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace SistemaLojaDeRoupas.Repository
+{
+    public interface IRepository<T>
+    {
+        T GetById(int id);
+        IEnumerable<T> GetAll();
+        T Add(T entity);
+    }
+}
