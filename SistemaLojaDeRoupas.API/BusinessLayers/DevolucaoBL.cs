bc88e4753553d66dcb7e9dbaf01b133e6f2904c5
little-junior <rampagedolol@outlook.com>
2024-02-21 22:34:38 -0300

    feat: add BusinessLayer folder and classes

---
+++ SistemaLojaDeRoupas.API/BusinessLayers/DevolucaoBL.cs
@@ -0,0 +1,44 @@
+﻿using SistemaLojaDeRoupas.API.CustomExceptions;
+using SistemaLojaDeRoupas.API.RequestDTOs;
+using SistemaLojaDeRoupas.Models;
+using SistemaLojaDeRoupas.Repository;
+
+namespace SistemaLojaDeRoupas.API.BusinessLayers
+{
+    public class DevolucaoBL
+    {
+        private readonly IRepository<Venda> _vendaRepository;
+
+        public DevolucaoBL(IRepository<Venda> vendaRepository)
+        {
+            _vendaRepository = vendaRepository;
+        }
+
+        public void ValidateRequestProdutosQuantidade(DevolucaoRequest devolucaoRequest)
+        {
+            if (devolucaoRequest.ProdutosQuantidade.Count == 0)
+            {
+                throw new CustomException("The 'produtosQuantidade' field can't be empty", 400);
+            }
+        }
+
+        public void ValidateRequestMotivo(DevolucaoRequest devolucaoRequest)
+        {
+
+            if (String.IsNullOrEmpty(devolucaoRequest.Motivo))
+            {
+                throw new CustomException("The 'motivo' field can't be empty", 400);
+            }
+        }
+
+        public void ValidateRequestVendaId(DevolucaoRequest devolucaoRequest)
+        {
+            var vendaSearch = _vendaRepository.GetById(devolucaoRequest.VendaId);
+
+            if (vendaSearch == null)
+            {
+                throw new CustomException("There is not a 'venda' with the specified id", StatusCodes.Status400BadRequest);
+            }
+        }
+    }
+}
---
+++ SistemaLojaDeRoupas.API/BusinessLayers/VendaBL.cs
@@ -0,0 +1,61 @@
+﻿using SistemaLojaDeRoupas.API.CustomExceptions;
+using SistemaLojaDeRoupas.API.RequestDTOs;
+
+namespace SistemaLojaDeRoupas.API.BusinessLayers
+{
+    public class VendaBL
+    {
+        public void ValidateRequestProdutosQuantidade(VendaRequest vendaRequest)
+        {
+            if (vendaRequest.ProdutosQuantidade.Count == 0)
+            {
+                throw new CustomException("The 'produtosQuantidade' field can't be empty", 400);
+            }
+        }
+
+        public bool ValidateCpfCliente(VendaRequest vendaRequest)
+        {
+            var cpf = vendaRequest.CpfCliente;
+
+            int[] multiplicador1 = { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int[] multiplicador2 = { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+            string tempCpf;
+            string digito;
+            int soma;
+            int resto;
+
+            if (cpf.Length != 11)
+                return false;
+
+            tempCpf = cpf.Substring(0, 9);
+            soma = 0;
+
+            for (int i = 0; i < 9; i++)
+                soma += int.Parse(tempCpf[i].ToString()) * multiplicador1[i];
+
+            resto = soma % 11;
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+
+            digito = resto.ToString();
+
+            tempCpf += digito;
+
+            soma = 0;
+            for (int i = 0; i < 10; i++)
+                soma += int.Parse(tempCpf[i].ToString()) * multiplicador2[i];
+
+            resto = soma % 11;
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+
+            digito += resto.ToString();
+
+            return cpf.EndsWith(digito);
+        }
+    }
+}
