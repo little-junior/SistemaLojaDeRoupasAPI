a38a2aadd901114a211e31f5d6b9042a025306d3
little-junior <rampagedolol@outlook.com>
2024-02-21 22:34:54 -0300

    feat: add custom exception

---
+++ SistemaLojaDeRoupas.API/CustomExceptions/CustomException.cs
@@ -0,0 +1,11 @@
+﻿namespace SistemaLojaDeRoupas.API.CustomExceptions
+{
+    public class CustomException : Exception
+    {
+        public int StatusCode { get; set; }
+        public CustomException(string message, int statusCode) : base(message) 
+        {
+            StatusCode = statusCode;
+        }
+    }
+}
