d66cac544fab2a65bc4669198c063bb74d2e68e4
little-junior <rampagedolol@outlook.com>
2024-02-21 22:39:01 -0300

    feat: implement Exception filter

--- SistemaLojaDeRoupas.API/Filters/ExceptionFilter.cs
+++ SistemaLojaDeRoupas.API/Filters/ExceptionFilter.cs
@@ -1,12 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc.Filters;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using SistemaLojaDeRoupas.API.ResponseDTOs;
+using SistemaLojaDeRoupas.API.CustomExceptions;
 
 namespace SistemaLojaDeRoupas.API.Filters
 {
     public class ExceptionFilter : IExceptionFilter
     {
+        private readonly ILogger<ExceptionFilter> _logger;
+
+        public ExceptionFilter(ILogger<ExceptionFilter> logger)
+        {
+            _logger = logger;
+        }
+
         public void OnException(ExceptionContext context)
         {
-            
+            var exceptionResponse = new ExceptionResponse()
+            {
+                SystemLog = "An exception was thrown by the system",
+                Message = context.Exception.Message
+            };
+
+            if (context.Exception is CustomException customException)
+                context.HttpContext.Response.StatusCode = customException.StatusCode;
+            else
+                context.HttpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+
+            context.Result = new JsonResult(exceptionResponse) { };
+
+            _logger.LogError($"ERROR: {context.Exception.Message}");
         }
     }
 }
